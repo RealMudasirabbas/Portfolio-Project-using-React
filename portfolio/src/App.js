bad5ad31c94c3dc87a71464da6c23fb9089c8c6b
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2023-12-12 23:25:13 +0500

    add them

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,22 @@
-import './App.scss';
-import Navbar from './Components/Navbar';
-
+import "./App.scss";
+import Links from "./Components/Links";
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <div>
-     <section>
-      <Navbar/>
       
-     </section>
-     <section>Parallax</section>
-     <section>Services</section>
-     <section>Parallax</section>
-     <section>Portfolio1</section>
-     <section>Portfolio2</section>
-     <section>Portfolio3</section>
-     <section>Contact</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 }
